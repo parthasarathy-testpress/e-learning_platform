a214b428330bdc22e48b659c74ca07f25551e51d
parthasarathy-testpress <parthasarathy@testpress.in>
2025-09-22 15:35:23 +0530

    feat: Add courses list and detail page

--- courses/views.py
+++ courses/views.py
@@ -10,7 +10,9 @@ from django.apps import apps
 from .models import Module, Content
 from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
 from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
-
+from django.db.models import Count
+from .models import Subject
+from django.views.generic.detail import DetailView
 class OwnerMixin(LoginRequiredMixin, PermissionRequiredMixin):
     def get_queryset(self):
         qs = super().get_queryset()
@@ -171,3 +173,23 @@ class ContentOrderView(CsrfExemptMixin,JsonRequestResponseMixin, View):
   for id, order in self.request_json.items():
    Content.objects.filter(id=id, module__course__owner=request.user).update(order=order)
   return self.render_json_response({'saved': 'OK'}) 
+ 
+
+class CourseListView(TemplateResponseMixin, View):
+  model = Course
+  template_name = 'courses/course/list.html'
+  def get(self, request, subject=None):
+   subjects = Subject.objects.annotate(
+   total_courses=Count('courses'))
+   courses = Course.objects.annotate(
+   total_modules=Count('modules'))
+   if subject:
+    subject = get_object_or_404(Subject, slug=subject)
+    courses = courses.filter(subject=subject)
+   return self.render_to_response({'subjects': subjects,
+    'subject': subject,
+    'courses': courses})  
+   
+class CourseDetailView(DetailView):
+ model = Course
+ template_name = 'courses/course/detail.html'
